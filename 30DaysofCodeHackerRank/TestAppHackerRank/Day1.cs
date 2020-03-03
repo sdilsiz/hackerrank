32706beb414a4f29b7fb1384ddeee7af364f2909
sdilsiz <sdilsiz@users.noreply.github.com>
2020-03-03 22:45:08 +0100

    day2

---
+++ 30DaysofCodeHackerRank/TestAppHackerRank/Day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppHackerRank {
+    public class Day1 {
+
+
+        public static void Run() {
+
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            // Declare second integer, double, and String variables.
+            // Declare  variables: one of type int, one of type double, and one of type String.
+            int intvariable = 12;
+            double doublevariable = 4.0;
+            string stringvariable = "is the best place to learn and practice coding!";
+            // Read and save an integer, double, and String to your variables.
+            //intvariable=Convert.ToInt32(Console.ReadLine());
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine((intvariable + i).ToString());
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(string.Format("{0:0.0}", doublevariable + d));
+            // Concatenate and print the String variables on a new line
+            Console.WriteLine(s + stringvariable);
+            // The 's' variable above should be printed first.
+
+            intvariable = Convert.ToInt32(Console.ReadLine());
+            doublevariable = Convert.ToDouble(Console.ReadLine());
+            stringvariable = Console.ReadLine();
+
+            Console.WriteLine((intvariable + i).ToString());
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(string.Format("{0:0.0}", doublevariable + d));
+            // Concatenate and print the String variables on a new line
+            Console.WriteLine(s + stringvariable);
+        }
+    }
+}
---
+++ 30DaysofCodeHackerRank/TestAppHackerRank/Day2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppHackerRank {
+    public class Day2 {
+        public static void Run() {
+
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            solve(meal_cost, tip_percent, tax_percent);
+
+        }
+
+        static void solve(double meal_cost, int tip_percent, int tax_percent) {
+
+            double tip = meal_cost * tip_percent / 100;
+            double tax = meal_cost * tax_percent / 100;
+            Console.WriteLine(Convert.ToInt32(tip + tax + meal_cost));
+        }
+    }
+}
